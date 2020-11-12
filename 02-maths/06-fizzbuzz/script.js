10ef7b21c10b19e3fddef27dfae922eca0248281
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-12 13:44:44 +0100

    Solved 0S-fizzbuzz

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -9,8 +9,22 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
     // your code here
+// .addEventListener('load', pageOpened, function promptBox() {
 
-})();
+function fizzBuzz(i) {
+    // for (var i = 1; i <= 100; i){
+        if (i%3 === 0 && i%5 === 0 && i>= 1 && i<=100) {
+            console.log('Fizzbuzz');
+        }
+        else if (i%3 === 0 && i>= 1 && i<=100){
+            console.log('Fizz');
+        }
+        else if (i%5 === 0 && i>= 1 && i<=100){
+            console.log('Buzz');
+        }
+        else{
+            console.log (i);
+        }
+}
