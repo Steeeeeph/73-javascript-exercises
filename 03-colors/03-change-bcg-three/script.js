7af8580b78c990529bec4d6aeaf5daa0b9232c9c
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-12 16:36:41 +0100

    Solved 03-change-bgc-three

--- 03-colors/03-change-bcg-three/script.js
+++ 03-colors/03-change-bcg-three/script.js
@@ -9,8 +9,24 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+ // your code here
+//  const randomColor = Math.floor(Math.random()*1000000);
+//  console.log(randomColor);
 
-})();
+ function randomNum(){
+     return Math.floor(Math.random()*16777215).toString(16);
+ }
+ let randomColor ='#'+ randomNum();
+ console.log(randomColor)
+document.getElementById("run").addEventListener('click', function(){
+ document.body.style.backgroundColor = randomColor;
+   
+})
+/*
+ function getRandomNumberBetween(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+//usage example: getRandomNumberBetween(20,400); 
+*/
