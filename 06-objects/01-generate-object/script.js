f31e003c17741483a3d97bd6dd72a0e240574288
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-13 12:02:05 +0100

    Solved 01 of objects

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -8,7 +8,9 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-
-(() => {
     // your code here
-})();
+
+const steph ={firstname:'Stephanie', lastname:'Van Bockhaven', age:32, city:'Ghent', country:'Belgium'};
+document.getElementById("run").addEventListener("click", function(){
+    console.log('Hi! I\'m '+steph.firstname+' '+steph.lastname+'. I\'m '+steph.age+' years old and I live in '+steph.city+', located in '+steph.country+'.')
+});
