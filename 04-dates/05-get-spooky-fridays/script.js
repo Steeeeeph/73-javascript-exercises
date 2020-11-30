d07d1feab104f0c6873bba38efc25348fe684dac
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2020-11-30 11:18:46 +0100

    Add folder 9, solved 9.1

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -11,6 +11,41 @@
 
 (function() {
 
-    // your code here
+    const months = {
+        0: 'January',
+        1: 'February',
+        2: 'March',
+        3: 'April',
+        4: 'May',
+        5: 'June',
+        6: 'July',
+        7: 'August',
+        8: 'September',
+        9: 'October',
+        10: 'November',
+        11: 'December'
+    }
+    const d = new Date();
 
+    document.getElementById("run").addEventListener('click', function (){
+        const yearInput = Number(document.getElementById("year").value);
+
+        // let day = d.getDay()
+        // let dayNumber = d.getDate();
+        let listOfUnfortunateMonths = [];
+
+        for (let i = 0; i <= 11; i++) {
+            d.setMonth();
+            console.log(d.setMonth(i));
+
+            // if(day == 5 && dayNumber == 13) {
+            //     console.log('Yep');
+            // }
+
+
+        }
+
+    });
+
+    
 })();
